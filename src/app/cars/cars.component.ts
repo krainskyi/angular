378624d04232c36377d911c7a5a9bbd905a2d8c7
Yura <47417811+krainskyi@users.noreply.github.com>
2019-07-04 17:22:33 +0300

    test_02

---
+++ src/app/cars/cars.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+@Component({
+  selector: 'app-cars',
+  templateUrl: './cars.component.html',
+  styleUrls: ['./cars.component.css']
+})
+export class CarsComponent implements OnInit {
+
+  constructor( ) { }
+
+  ngOnInit() {
+  }
+
+
+}
+const add =[];

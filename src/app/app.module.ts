3821b7b4c0fa9ca777d8d3f9edc49f204039b39a
Yura <47417811+krainskyi@users.noreply.github.com>
2019-07-04 23:49:16 +0300

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarsComponent } from './cars/cars.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,11 +12,9 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }
--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,17 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder} from '@angular/forms';
+
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
   styleUrls: ['./cars.component.css']
 })
 export class CarsComponent implements OnInit {
-
-  constructor( ) { }
+  todos = [
+    {
+      brand: 'Toyota',
+      new: false,
+      used: true,
+      year: 2014,
+      price: 15000
+    }
+    ];
+  constructor() { }
 
   ngOnInit() {
+
   }
-
-
+  addTodo(newTodoBrand) {
+    let newTodo = {
+      brand: newTodoBrand,
+      new: newTodoBrand,
+      used: newTodoBrand,
+      year: newTodoBrand,
+      price: newTodoBrand
+    };
+    this.todos.push(newTodo);
+    
+  }
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  addHero(newHero: string) {
+    if (newHero) {
+      this.heroes.push(newHero);
+    }
+  }
 }
-const add =[];

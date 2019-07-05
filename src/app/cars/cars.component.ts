ac9f3099aff5fb13356254d227afa97b4ca8f3d4
Yura <47417811+krainskyi@users.noreply.github.com>
2019-07-05 23:45:05 +0300

    h/w -add newcar

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,6 +1,8 @@
 import { element } from 'protractor';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { disableDebugTools } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-cars',
@@ -8,27 +10,33 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./cars.component.css']
 })
 export class CarsComponent implements OnInit {
-  
+
   public cars: any = [];
-  public car = {brand:'', price: '', year:'', used:false};
+
+  public car = { brand: '', price: '', year: '', used: false };
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  addCar(newCar){
+  addCar(newCar) {
+    if (!newCar.used == false) {
+      newCar.year = '2019' ;
+    }
     this.cars.push(newCar);
     console.log(newCar);
-  }  
-  
+  }
+
+}
   // heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
   // addHero(newHero: string) {
   //   if (newHero) {
   //     this.heroes.push(newHero);
   //   }
   // }
-  
-}
+
+
 
 
 
